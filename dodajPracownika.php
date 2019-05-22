750266d94458a3ec90b151b7630c97a108676351
Bartłomiej Sopata <sopatabartek@gmail.com>
2019-05-22 18:10:32 +0000

    Update dodajPracownika.php

---
+++ dodajPracownika.php
@@ -0,0 +1,229 @@
+<?php
+
+	session_start();   
+    require_once "connect.php";
+
+    
+	if (isset($_POST['imie']))
+    {   $wszystko_OK=true;
+
+		$imie = $_POST['imie'];}
+
+
+if (isset($_POST['nazwisko']))
+    {   $wszystko_OK=true;
+
+		$nazwisko = $_POST['nazwisko'];}
+
+	if (isset($_POST['email']))
+	{
+		$wszystko_OK=true;
+
+		$nick = $_POST['nick'];
+		
+		if ((strlen($nick)<3) || (strlen($nick)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="nazwa użytkownika musi posiadać od 3 do 20 znaków!";
+		}
+		
+		//if (ctype_alnum($nick)==false)
+		//{
+		//	$wszystko_OK=false;
+		//	$_SESSION['e_nick']="Nick może składać się tylko z liter i cyfr (bez polskich znaków)";
+		//}
+		
+		$email = $_POST['email'];
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_email']="Podaj poprawny adres e-mail!";
+		}
+		
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
+		}
+		
+		if ($haslo1!=$haslo2)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
+		}	
+
+		$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+		
+		//if (!isset($_POST['regulamin']))
+		/*{
+			$wszystko_OK=false;
+			$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
+		}*/				
+		
+		$_SESSION['fr_nick'] = $nick;
+		$_SESSION['fr_email'] = $email;
+		$_SESSION['fr_haslo1'] = $haslo1;
+		$_SESSION['fr_haslo2'] = $haslo2;
+		//if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
+		
+		require_once "connect.php";
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		
+		try 
+		{
+			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+			if ($polaczenie->connect_errno!=0)
+			{
+				throw new Exception(mysqli_connect_errno());
+			}
+			else
+			{
+				$rezultat = $polaczenie->query("SELECT DAN_ID FROM dane WHERE DAN_EMAIL='$email'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_maili = $rezultat->num_rows;
+				if($ile_takich_maili>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_email']="taki email juz istnieje w bazie";
+				}		
+
+				$rezultat = $polaczenie->query("SELECT DAN_ID FROM dane WHERE DAN_LOGIN='$nick'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_nickow = $rezultat->num_rows;
+				if($ile_takich_nickow>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_nick']="Taki pracownik juz istnieje";
+				}
+				
+				if ($wszystko_OK==true)
+				{
+					
+					if ($polaczenie->query("INSERT INTO firma_transportowa.dane VALUES (NULL, '$imie','$nazwisko', '$email','$nick','$haslo_hash', 1)"))
+					{
+						$_SESSION['udanarejestracja']=true;
+						header('Location: index.php');
+					}
+					else
+					{
+						throw new Exception($polaczenie->error);
+					}
+					
+				}
+				
+				$polaczenie->close();
+			}
+			
+		}
+		catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera!</span>';
+			echo '<br />Informacja developerska: '.$e;
+		}
+		
+	}
+	
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>busy - Rejestracja</title>
+	
+	<style>
+		.error
+		{
+			color:red;
+			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+	</style>
+</head>
+
+<body>
+	
+	<form method="post">
+	
+		Nickname: <br /> <input type="text" value="<?php
+			if (isset($_SESSION['fr_nick']))
+			{
+				echo $_SESSION['fr_nick'];
+				unset($_SESSION['fr_nick']);
+			}
+		?>" name="nick" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_nick']))
+			{
+				echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+				unset($_SESSION['e_nick']);
+			}
+		?>
+		
+		Imie: <br /> <input type="text" name="imie" /><br />
+				
+        Nazwisko: <br /> <input type="text" name="nazwisko" /><br />
+		
+		
+		E-mail: <br /> <input type="text" value="<?php
+			if (isset($_SESSION['fr_email']))
+			{
+				echo $_SESSION['fr_email'];
+				unset($_SESSION['fr_email']);
+			}
+		?>" name="email" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_email']))
+			{
+				echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+				unset($_SESSION['e_email']);
+			}
+		?>
+		
+		Twoje hasło: <br /> <input type="password"  value="<?php
+			if (isset($_SESSION['fr_haslo1']))
+			{
+				echo $_SESSION['fr_haslo1'];
+				unset($_SESSION['fr_haslo1']);
+			}
+		?>" name="haslo1" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_haslo']))
+			{
+				echo '<div class="error">'.$_SESSION['e_haslo'].'</div>';
+				unset($_SESSION['e_haslo']);
+			}
+		?>		
+		
+		Powtórz hasło: <br /> <input type="password" value="<?php
+			if (isset($_SESSION['fr_haslo2']))
+			{
+				echo $_SESSION['fr_haslo2'];
+				unset($_SESSION['fr_haslo2']);
+			}
+		?>" name="haslo2" /><br />
+		
+
+		
+		<br />
+		
+		<input type="submit" value="Zarejestruj pracownika" />
+		
+	</form>
+
+</body>
+</html>
